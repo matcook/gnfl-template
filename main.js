125e170d299a65cac9c879d1adcad835d738a3f4
Mathew Cook <mat.cook@bigpond.com>
2021-05-12 13:14:53 +0800

    started sponsors carousel

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
+// HERO AREA SLIDESHOW
 
+let slideshowImages = document.querySelectorAll('.slide-image');
+let slideIndex = 1;
+
+if (slideshowImages.length > 0) {
+  const playSlides = setInterval(() => {
+    nextSlide();
+  }, 5000);
+}
+
+const nextSlide = () => {
+  if (slideIndex == slideshowImages.length) {
+    slideIndex = 0;
+    slideshowImages[slideshowImages.length - 1].classList.remove('is-visible');
+  }
+  if (slideIndex > 0) {
+    slideshowImages[slideIndex - 1].classList.remove('is-visible');
+  }
+  slideshowImages[slideIndex].classList.add('is-visible');
+  slideIndex++;
+};
